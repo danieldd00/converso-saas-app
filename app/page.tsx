2ded2fabcb95e96af0556a4bb9257f06302ece2c
Daniel Dizdarevic <daniel@threedark.com>
2025-05-26 15:40:03 -0500

    Finalize app

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,24 @@ import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
 import { recentSessions } from "@/constants";
+import { getAllCompanions, getRecentSessions } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1>Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard id="123" name="Neura the Brainy Explorer" topic="Neural Network of the Brain" subject="science" duration={45} color="#ffda6e" />
-        <CompanionCard id="456" name="Countsy the Number Wizard" topic="Derivatives & Integrals" subject="maths" duration={30} color="#e5d0ff" />
-        <CompanionCard id="789" name="Verba the Vocabulary Builder" topic="language" subject="English Literature" duration={30} color="#bde7ff" />
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        ))}
       </section>
 
       <section className="home-section">
-        <CompanionsList title="Recently completed sessions" companions={recentSessions} classNames="w-2/3 max-lg:w-full " />
+        <CompanionsList title="Recently completed sessions" companions={recentSessionCompanions} classNames="w-2/3 max-lg:w-full " />
         <CTA />
       </section>
     </main>
